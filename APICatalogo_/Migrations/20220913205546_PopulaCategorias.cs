a62d5181b919cba50cac27d7b9b156d8d1b091fc
Diego Nobre <diegonobre.dev@outlook.com>
2022-09-13 19:15:25 -0300

    Tabelas funcionando

--- APICatalogo_/Migrations/20220913205546_PopulaCategorias.cs
+++ APICatalogo_/Migrations/20220913205546_PopulaCategorias.cs
@@ -6,14 +6,16 @@ namespace APICatalogo_.Migrations
 {
     public partial class PopulaCategorias : Migration
     {
-        protected override void Up(MigrationBuilder migrationBuilder)
+        protected override void Up(MigrationBuilder mb)
         {
-
+            mb.Sql("Insert into Categorias(Nome, ImagemUrl) Values('Bebidas','bebidas.jpg')");
+            mb.Sql("Insert into Categorias(Nome, ImagemUrl) Values('Lanches','lanches.jpg')");
+            mb.Sql("Insert into Categorias(Nome, ImagemUrl) Values('Sobremesas','sobremesas.jpg')");
         }
 
-        protected override void Down(MigrationBuilder migrationBuilder)
+        protected override void Down(MigrationBuilder mb)
         {
-
+            mb.Sql("Delete from Categorias");
         }
     }
 }
